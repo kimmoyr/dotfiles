9c58e8eedb99f0c9f9685fbb0e4a1a463e991b79
Kimmo Yli-Rohdainen <kimmoyr@gmail.com>
2012-11-25 18:34:40 +0200

    Start Emacs daemon if it's not running when using emacsclient aliases.

--- emacs/aliases.zsh
+++ emacs/aliases.zsh
@@ -1,6 +1,6 @@
-alias ec='emacsclient -c -n -a nano'
-alias et='emacsclient -t -a nano'
-alias em='emacsclient -n -a nano'
-ecs() { emacsclient -c -n -a emacs "/sudo::$*" }
-ets() { emacsclient -t -a emacs "/sudo::$*" }
-ems() { emacsclient -n -a emacs "/sudo::$*" }
+alias ec='emacsclient -c -n -a ""'
+alias et='emacsclient -t -a ""'
+alias em='emacsclient -n -a ""'
+ecs() { emacsclient -c -n -a "" "/sudo::$*" }
+ets() { emacsclient -t -a "" "/sudo::$*" }
+ems() { emacsclient -n -a "" "/sudo::$*" }
